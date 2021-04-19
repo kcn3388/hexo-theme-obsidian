6e3a07702c9057959e864c5f245be3301514e647
kcn3388 <kcn3388@126.com>
2021-04-19 17:08:06 +0800

    fix relative path

--- source/live2d-widget/autoload.js
+++ source/live2d-widget/autoload.js
@@ -36,7 +36,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "http://localhost:4000/live2d_api/",
-			cdnPath: "../../live2d_api/"
+			cdnPath: root_domain + "/live2d_api/"
 		});
 	});
 }
