a0ed07798716eca8f95e1bdc689f501ef6601e44
kcn3388 <kcn3388@126.com>
2021-04-19 16:54:42 +0800

    fix relative path

--- source/live2d-widget/autoload.js
+++ source/live2d-widget/autoload.js
@@ -1,5 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "../../live2d-widget/";
+var root_domain = "http://" + window.location.host;
+const live2d_path = root_domain + "/live2d-widget/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
