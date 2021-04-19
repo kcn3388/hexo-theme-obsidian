a8d1f04ddb542b41c1d2decd37ab5a6491a0c248
kcn3388 <kcn3388@126.com>
2021-04-19 16:54:42 +0800

    fix relative path

---
+++ source/js/loadaplayer.js
@@ -0,0 +1,4 @@
+var aplayer_css = root_domain + "/aplayer/dist/APlayer.min.css"
+var aplayer_js = root_domain + "/aplayer/dist/APlayer.min.js"
+var meeting_js = root_domain + "/js/Meting.min.js"
+var loadaplayer = "<link rel=\"stylesheet\" href=\"" + aplayer_css + "\"><script src=\""+ aplayer_js +"\"></script><script src=\""+ meeting_js +"\"></script>"
