d566b200c143f3d4caab8ffa1cf54ab418f04f37
kcn3388 <kcn3388@126.com>
2021-05-17 10:38:49 +0800

    fix hokusai.js

--- src/js/hokusai.js
+++ src/js/hokusai.js
@@ -1,7 +1,7 @@
 console.log("%c+",
 `font-size: 1px;
 padding: 122px 217px;
-background-image: url(` + root_domain + `/img/default-cover.jpg);
+background-image: url(https://kcn3388.club/hexo-theme-obsidian/src/img/default-cover.jpg);
 background-size: contain;
 background-repeat: no-repeat;
 color: transparent;`);
