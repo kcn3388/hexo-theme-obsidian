a836ab9303d6126481c13c427fdc694a62561262
kcn3388 <kcn3388@126.com>
2021-05-20 14:36:07 +0800

    change request to fetch

--- src/js/aboutmusic.js
+++ src/js/aboutmusic.js
@@ -3,13 +3,10 @@ String.prototype.splice = function(start, newStr) {
 };
 if (typeof (recURL) == "undefined") {
     var recURL
-    var recRequest = new XMLHttpRequest;
     var recData
     var httpsurl
-    var newmusicRequest = new XMLHttpRequest;
     var newmusicData
     var newmusicURL
-    var loginRequest = new XMLHttpRequest;
     var loginData
     var loginURL
     var index
@@ -17,52 +14,58 @@ if (typeof (recURL) == "undefined") {
 
 loginURL = "//api.kcn3388.club/netease/login/login?email=kcn3388@126.com&md5_password=1723e79b321daddbcfcb0ead23309120&timestamp=" + Date.now()
 
-loginRequest.onreadystatechange = function () {
-    if (loginRequest.readyState == 4 && loginRequest.status == 200) {
-        loginData = JSON.parse(loginRequest.responseText);
-        // console.log(loginData)
-        recURL = "//api.kcn3388.club/netease/recommend/songs?cookie=" + loginData.cookie
-        recRequest.open('GET', recURL);
-        recRequest.send();
-    }
-}
+login();
 
-loginRequest.open('POST', loginURL);
-loginRequest.send();
-
-newmusicRequest.onreadystatechange = function () {
-    if (newmusicRequest.readyState == 4 && newmusicRequest.status == 200) {
-        newmusicData = JSON.parse(newmusicRequest.responseText);
-        // console.log(newmusicData.data[0]);
-        httpsurl = newmusicData.data[0].url.splice(4, "s");
-        // console.log(httpsurl)
-        const ap1 = new APlayer({
-            element: document.getElementById('dailyplayer'),
-            mini: false,
-            autoplay: false,
-            lrcType: false,
-            mutex: true,
-            preload: 'metadata',
-            audio: [{
-                name: recData.data.dailySongs[index].name,
-                artist: recData.data.dailySongs[index].ar[0].name,
-                url: httpsurl,
-                cover: recData.data.dailySongs[index].al.picUrl,
-                theme: '#ebd0c2'
-            }]
+function login() {
+    fetch(loginURL)
+        .then(response => response.json())
+        .then(result => {
+            loginData = result;
+            if (loginData.code == 200) {
+                fetchrec();
+            }
         });
-    }
 }
 
-recRequest.onreadystatechange = function () {
-    if (recRequest.readyState == 4 && recRequest.status == 200) {
-        recData = JSON.parse(recRequest.responseText);
-        // console.log(recData.data.dailySongs[0])
-        index = randomNum(0, recData.data.dailySongs.length-1)
-        newmusicURL = "//api.kcn3388.club/netease/song/url?id=" + recData.data.dailySongs[index].id
-        newmusicRequest.open('GET', newmusicURL);
-        newmusicRequest.send();
-    }
+function fetchrec() {
+    recURL = "//api.kcn3388.club/netease/recommend/songs?cookie=" + loginData.cookie
+    fetch(recURL)
+        .then(response => response.json())
+        .then(result => {
+            recData = result;
+            if (recData.code == 200) {
+                fetchnewsong();
+            }
+        });
+}
+
+function fetchnewsong() {
+    index = randomNum(0, recData.data.dailySongs.length-1)
+    newmusicURL = "//api.kcn3388.club/netease/song/url?id=" + recData.data.dailySongs[index].id
+    fetch(newmusicURL)
+        .then(response => response.json())
+        .then(result => {
+            newmusicData = result;
+            if (newmusicData.code == 200) {
+                httpsurl = newmusicData.data[0].url.splice(4, "s");
+                // console.log(httpsurl)
+                const ap1 = new APlayer({
+                    element: document.getElementById('dailyplayer'),
+                    mini: false,
+                    autoplay: false,
+                    lrcType: false,
+                    mutex: true,
+                    preload: 'metadata',
+                    audio: [{
+                        name: recData.data.dailySongs[index].name,
+                        artist: recData.data.dailySongs[index].ar[0].name,
+                        url: httpsurl,
+                        cover: recData.data.dailySongs[index].al.picUrl,
+                        theme: '#ebd0c2'
+                    }]
+                });
+            }
+        });
 }
 
 //生成从minNum到maxNum的随机数
