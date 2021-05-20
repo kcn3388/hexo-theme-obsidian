71128ca602a2e80d339f55b9cf33cdda8b05a6fc
kcn3388 <kcn3388@126.com>
2021-05-20 14:36:07 +0800

    change request to fetch

--- src/js/pagemusic.js
+++ src/js/pagemusic.js
@@ -5,8 +5,6 @@ var e = document.getElementById("musicid")
 if (typeof (musicURL) == "undefined") {
     var musicURL
     var detailURL
-    var musicRequest = new XMLHttpRequest;
-    var dataRequest = new XMLHttpRequest;
     var musicData
     var detailData
     var httpsurl
@@ -15,38 +13,42 @@ if (typeof (musicURL) == "undefined") {
 musicURL = "//api.kcn3388.club/netease/song/url?id=" + e.className
 detailURL = "//api.kcn3388.club/netease/song/detail?ids=" + e.className
 
-musicRequest.onreadystatechange = function () {
-    if (musicRequest.readyState == 4 && musicRequest.status == 200) {
-        musicData = JSON.parse(musicRequest.responseText);
-        // console.log(musicData.data[0]);
-        httpsurl = musicData.data[0].url.splice(4, "s");
-        // console.log(httpsurl)
-    }
-    dataRequest.open('GET', detailURL);
-    dataRequest.send();
+fetchsong();
+
+function fetchsong() {
+    fetch(musicURL)
+        .then(response => response.json())
+        .then(result => {
+            musicData = result;
+            if (musicData.code == 200) {
+                httpsurl = musicData.data[0].url.splice(4, "s");
+                // console.log(httpsurl)
+                fetchdetails();
+            }
+        });
 }
 
-musicRequest.open('GET', musicURL);
-musicRequest.send();
-
-dataRequest.onreadystatechange = function () {
-    if (dataRequest.readyState == 4 && dataRequest.status == 200) {
-        detailData = JSON.parse(dataRequest.responseText);
-        // console.log(detailData.songs[0])
-        const ap1 = new APlayer({
-            element: document.getElementById('player1'),
-            mini: false,
-            autoplay: false,
-            lrcType: false,
-            mutex: true,
-            preload: 'metadata',
-            audio: [{
-                name: detailData.songs[0].name,
-                artist: detailData.songs[0].ar[0].name,
-                url: httpsurl,
-                cover: detailData.songs[0].al.picUrl,
-                theme: '#ebd0c2'
-            }]
+function fetchdetails() {
+    fetch(detailURL)
+        .then(response => response.json())
+        .then(result => {
+            detailData = result;
+            if (detailData.code == 200) {
+                const ap1 = new APlayer({
+                    element: document.getElementById('player1'),
+                    mini: false,
+                    autoplay: false,
+                    lrcType: false,
+                    mutex: true,
+                    preload: 'metadata',
+                    audio: [{
+                        name: detailData.songs[0].name,
+                        artist: detailData.songs[0].ar[0].name,
+                        url: httpsurl,
+                        cover: detailData.songs[0].al.picUrl,
+                        theme: '#ebd0c2'
+                    }]
+                });
+            }
         });
-    }
 }
