73ec0aa6242e12a1de132879f3648c9fc12b0bf7
kcn388 <kcn3388@126.com>
2022-02-18 03:59:38 +0800

    change css and js cdn

--- src/live2d-widget/waifu-tips.js
+++ src/live2d-widget/waifu-tips.js
@@ -22,15 +22,15 @@ function loadWidget(config) {
 			<div id="waifu-tips"></div>
 			<canvas id="live2d" width="800" height="800"></canvas>
 			<div id="waifu-tool">
-				<span class="fa fa-regular fa-comment"></span>
-				<span class="fa fa-regular fa-paper-plane"></span>
-				<span class="fa fa-regular fa-user-circle"></span>
-				<span class="fa fa-regular fa-street-view"></span>
-				<span class="fa fa-regular fa-camera-retro"></span>
-				<span class="fa fa-regular fa-info-circle"></span>
-				<span class="fa fa-regular fa-times"></span>
-				<span class="fa fa-regular fa-chevron-right" id="live2d-go-right"></span>
-                <span class="fa fa-regular fa-chevron-left" id="live2d-go-left"></span>
+				<span class="fa fa-lg fa-comment"></span>
+				<span class="fa fa-lg fa-paper-plane"></span>
+				<span class="fa fa-lg fa-user-circle"></span>
+				<span class="fa fa-lg fa-street-view"></span>
+				<span class="fa fa-lg fa-camera-retro"></span>
+				<span class="fa fa-lg fa-info-circle"></span>
+				<span class="fa fa-lg fa-times"></span>
+				<span class="fa fa-lg fa-chevron-right" id="live2d-go-right"></span>
+                <span class="fa fa-lg fa-chevron-left" id="live2d-go-left"></span>
 			</div>
 		</div>`);
 	// https://stackoverflow.com/questions/24148403/trigger-css-transition-on-appended-element
